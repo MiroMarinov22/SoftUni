f1fde062e371c2724e7f5e068d89aaa38ea10422
Miroslav <miroslav.jorov22@gmail.com>
2024-03-25 22:16:44 +0200

    Homework - Objects and Classes

---
+++ Programming Advanced for QA/Objects and Classes - Exercise/1. Students/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _1._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfStudents = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < countOfStudents; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string firstName = input[0];
+                string lastName = input[1];
+                double grade = double.Parse(input[2]);
+
+                Student student = new Student();
+                {
+                    student.FirstName = firstName;
+                    student.LastName = lastName;
+                    student.Grade = grade;
+                }
+
+
+                students.Add(student);
+            }
+            foreach (Student student in students.OrderByDescending(student => student.Grade))
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");
+            }
+        }
+
+        public class Student
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public double Grade { get; set; }
+
+        }
+    }
+}
---
+++ Programming Advanced for QA/Objects and Classes - Exercise/2. Articles/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace _2._Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputData = Console.ReadLine().Split(", ");
+
+            string title = inputData[0];
+            string content = inputData[1];
+            string author = inputData[2];
+
+            Articles article = new Articles(title, content, author);
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] commandInfo = Console.ReadLine().Split(": ");
+
+                string command = commandInfo[0];
+                string data = commandInfo[1];
+
+                if (command == "Edit")
+                {
+                    article.Edit(data);
+                }
+
+                else if (command == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(data);
+                }
+
+                else if (command == "Rename")
+                {
+                    article.Rename(data);
+                }
+            }
+
+            Console.WriteLine(article);
+        }
+    }
+
+    public class Articles
+    {
+        public Articles(string title, string content, string author)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Edit(string newContent)
+        {
+            this.Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            this.Author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            this.Title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Content}: {this.Author}";
+        }
+    }
+}
